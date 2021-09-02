a92965bfb91c7e9e41903a2f3ab860854ec04bd1
Soham Dasgupta <59978919+astonishingwolf@users.noreply.github.com>
2021-09-02 20:19:42 +0530

    policy_gradient

--- Lunar_Landing/policy_network.py
+++ Lunar_Landing/policy_network.py
@@ -31,4 +31,54 @@ class PolicyNetwork(nn.Module):
         x = self.fc3(x)
         return x
     
+
+class PolicyGradientAgent():
+    
+    def __init__(self,lr, input_dims , gamma=0.99 , n_action =4):
+        self.gamma = gamma
+        self.lr =lr
+        self.reward_memory = []
+        self.action_memory = [] 
+        self.policy = PolicyNetwork(self.lr, input_dims, n_action)
         
+    def choose_action(self, observation):
+        state = T.Tensor([observation]).to(self.policy.device)
+        probabilities = F.softmax(self.policy.forward(state))
+        action_probs = T.distributions.Categorical(probabilities)
+        action = action_probs.sample
+        log_probs = action_probs.log_prob(action)
+        self.action_memory.append(log_probs)
+        
+        return action.reward()
+    
+    def store_reward(self,reward):
+        self.reward_memory.append(reward)
+        
+    def learn():
+        self.policy.optimizer.zero_grad()
+        
+        G = np.zero_like(self.reward_memory , dtype = np.float64)
+        for i in range(len(self.reward_memory)):
+            G_sum = 0
+            discount = 1
+            for k in range(i,len(self.reward_memory)):
+                G_sum+=self.reward_memory[k]*discount
+                discount+=self.gamma
+            G[i] = G_sum
+        G = T.tensor(G , dtype = T.float).to(self.policy.device)
+        
+        loss = 0
+        
+        for g, logprob in zip(G,self.action_memory):
+            loss += -g * logprob
+        loss.backward()
+        self.policy.optimizer.step()
+        
+        self.action_memory = []
+        self.reward_memory = []
+    
+        
+        
+        
+        
+                
